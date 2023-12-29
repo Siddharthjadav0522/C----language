c3ecff57c000b2e8a2d3078e597a637e70d6189f
Siddharthjadav0522 <149317287+Siddharthjadav0522@users.noreply.github.com>
2023-12-29 13:07:23 +0530

    Add files via upload

---
+++ leb work 2/work3.c
@@ -0,0 +1,46 @@
+#include<stdio.h>>
+int main(){
+    int a,b,c,d;
+    printf("enter a,b,c,d :");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+
+    // nested if else use of four velue in maximum
+    if (a>b){
+        if(a>c){
+            if(a>d){
+                printf("a is max");
+            }
+            else{
+                printf("d is max");
+            }
+        }
+        else{
+            if(c>d){
+                printf("c is max");
+            }
+            else{
+                printf("d is max");
+            }
+        }
+    }
+    else{ 
+         if(b>c){
+            if(b>d){
+                printf("b is max");
+            }
+            else{
+                printf("d is max");
+            }
+        }
+        else{
+             if(c>d){
+                printf("c is max");
+
+            }else{
+                printf("d is max");
+            }
+        }  
+    }
+    
+       return 0;
+}
